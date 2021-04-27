66d7a331a8cbac1aabc8c029ddd83e64a51ece84
Chris McCormick <chris@mccormick.cx>
2021-04-27 21:41:25 +0800

    Store sessions in kv.

--- src/clast/web.cljs
+++ src/clast/web.cljs
@@ -6,28 +6,51 @@
     ["keyv" :as Keyv]
     ["process" :as process]
     ["express" :as express]
+    ["events" :as EventEmitter]
     ["cookie-parser" :as cookies]
     ["body-parser" :as body-parser]
     ["serve-static" :as serve-static]
     ["express-session" :as session]
     ["morgan" :as morgan]
-    ["rotating-file-stream" :as rfs]))
+    ["rotating-file-stream" :as rfs]
+    [cljs.core.async :refer (go <!) :as async]
+    [cljs.core.async.interop :refer-macros [<p!]]))
+
+(defn create-store [kv]
+  (let [e (session/Store.)]
+    (aset e "destroy" (fn [sid callback]
+                        (go (<p! (j/call kv :destroy sid))
+                            (callback))))
+    (aset e "get" (fn [sid callback]
+                    (go (callback
+                          nil
+                          (<p! (j/call kv :get sid))))))
+    (aset e "set" (fn [sid session callback]
+                    (go (<p! (j/call kv :set sid session))
+                        (callback))))
+    (aset e "clear" (fn [callback]
+                      (go (<p! (js/call kv :clear))
+                          (callback))))
+    e))
 
 (defn add-default-middleware [app]
   ; set up logging
   (let [logs (str js/__dirname "/logs")
-        access-log (.createStream rfs "access.log" #js {:interval "7d" :path logs})]
-    (.use app (morgan "combined" #js {:stream access-log})))
+        access-log (.createStream rfs "access.log" #js {:interval "7d" :path logs})
+        kv-session (Keyv. (env "DATABASE" "sqlite://./rsstonews.sqlite") #js {:namespace "session"})
+        store (create-store kv-session)]
+    (.use app (morgan "combined" #js {:stream access-log}))
+    ; set up sessions table
+    (.use app (session #js {:secret (env "SECRET" "DEVMODE")
+                            :saveUninitialized false
+                            :resave true
+                            :cookie #js {:secure "auto"
+                                         :httpOnly true
+                                         ; 10 years
+                                         :maxAge (* 10 365 24 60 60 1000)}
+                            :store store})))
   ; configure sane server defaults
   (.set app "trust proxy" "loopback")
-  ; set up sessions table
-  (.use app (session #js {:secret (env "SECRET" "DEVMODE")
-                          :saveUninitialized false
-                          :resave true
-                          :cookie #js {:secure "auto"
-                                       :httpOnly true
-                                       ; 10 years
-                                       :maxAge (* 10 365 24 60 60 1000)}}))
   (.use app (cookies))
   ; json body parser
   (.use app (.json body-parser #js {:limit "10mb" :extended true :parameterLimit 1000}))
@@ -47,9 +70,9 @@
 (defn create []
   (let [kv (Keyv. (env "DATABASE" "sqlite://./rsstonews.sqlite"))]
     (-> (express)
-        (add-default-middleware)
         (j/assoc! :kv kv)
-        (j/assoc! :db (aget kv "opts" "store")))))
+        (j/assoc! :db (aget kv "opts" "store"))
+        (add-default-middleware))))
 
 (defn serve [app]
   (let [host (env "BIND_ADDRESS" "127.0.0.1")
