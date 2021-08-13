f336d8cc78702b2f0809f2a71c2738ec52f2232f
Chris McCormick <chris@mccormick.cx>
2021-08-13 10:28:59 +0800

    Backported fns from bots. Made it nbb friendly.

--- src/clast/util.cljs
+++ src/clast/util.cljs
@@ -1,5 +1,6 @@
 (ns clast.util
   (:require
+    ["path" :refer [basename]]
     ["util" :as util]
     ["caller-id" :as caller-id]
     ["chokidar" :as file-watcher]
@@ -19,10 +20,20 @@
     (aset js/console "log" log-fn)
     (aset js/console "error" log-fn)))
 
+(defn now []
+  (-> (js/Date.)
+      (.toISOString)
+      (.split ".")
+      first
+      (.replace "T" " ")))
+
 (defn bail [msg]
   (js/console.error msg)
   (js/console.error "Server exit.")
   (js/process.exit 1))
+
+(defn log [file-path & args]
+  (apply print (conj (conj args (str (basename file-path) ":")) (now))))
 
 (defn env [k & [default]]
   (or (aget js/process.env k) default))
@@ -49,8 +60,8 @@
 
 (defn build-absolute-uri [req path]
   (let [hostname (aget req "hostname")
-        host (aget req.headers "host")]
-    (str req.protocol "://"
+        host (aget req "headers" "host")]
+    (str (aget req "protocol") "://"
          (if (not= hostname "localhost") hostname host)
          (if (not= (aget path 0) "/") "/")
          path)))
