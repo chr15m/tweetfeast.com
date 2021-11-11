814d5d9a838763e94fd06745a15bcd3b7ef01e69
Chris McCormick <chris@mccormick.cx>
2021-11-11 12:53:46 +0800

    Search call type.

--- bin/call-type.cljs
+++ bin/call-type.cljs
@@ -13,9 +13,11 @@
                         (aset v "kind" k)
                         v)))]
     (doseq [row json]
-      (let [url (aget row "url")
-            is-name (when url (> (.indexOf url "users/by/username") -1))
-            paginated (when url (or (> (.indexOf url "pagination_token") -1)
-                                    (> (.indexOf url "next") -1)))]
-        (when (and url (not is-name) (not paginated))
-          (js/console.log (aget row "username") (-> url (.split "?") (aget 0) path/basename)))))))
+      (if (.startsWith (aget row "kind") "event/search")
+        (js/console.log (aget row "username") "search")
+        (let [url (aget row "url")
+              is-name (when url (> (.indexOf url "users/by/username") -1))
+              paginated (when url (or (> (.indexOf url "pagination_token") -1)
+                                      (> (.indexOf url "next") -1)))]
+          (when (and url (not is-name) (not paginated))
+            (js/console.log (aget row "username") (-> url (.split "?") (aget 0) path/basename))))))))
