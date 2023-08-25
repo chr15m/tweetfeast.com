8acdb8f0186e6005681c475ebe0459728ce882f3
Chris McCormick <chris@mccormick.cx>
2023-08-26 07:44:30 +0800

    Improve prompts with more reasoning.

--- src/twentiments/generator.cljs
+++ src/twentiments/generator.cljs
@@ -27,20 +27,24 @@
   "You are a helpful assistant.")
 
 (def prompt-1
-  "I'd like you to write some tweets for me.
+  "I'd like you to write some tweets for me that are designed to get a lot of engagement.
+
   I'm going to provide you with a list of tweets first and then I'm going to provide you with a topic or reference text to tweet about.
-  I want you to write fifteen (15) tweets in the same voice as the list of tweets I'll provide.
+  I want you to write fifteen (15) tweets in the same voice and style as the list of tweets I'll provide.
   It's important that the tweets you write are stylistically similar to the reference tweets.
   It's important that the tweets are on the topic of the reference text I'll provide.
 
   Please make the tweets smart, funny, entertaining, interesting, poignant and/or insightful.
+  Please use the tips and categories you listed above to write engaging tweets to inform the tweet writing process.
 
-  Here is the list of reference tweets. Tweets are delimited by three dashes.")
+  Here is the list of reference tweets. Tweets are delimited by three dashes:")
 
 (def prompt-2
   "Ok, now here is the topic or reference text I would like you to tweet about.
 
   Please generate fifteen (15) new tweets based on this topic or reference text.
+
+  Bear in mind the tweet writing strategies you listed above and try to make the tweets follow the tips listed in form and structure.
 
   Please follow these rules:
 
@@ -48,10 +52,64 @@
   - Please do not reply or use the 'R' shorthand.
   - Please do not reference other users with the at (@) symbol.
   - Please do not number the tweets.
+  - Please minimise the use of emojis unless they are used in the reference tweets.
+  - Please avoid inventing new hashtags. Minimise hashtag use.
+
+  Go into depth and write tweets that explain truths.
+  Use a calm, cool, and friendly tone without emojis and hashtags.
 
   Most importantly, please output the tweets in JSON format as an array of strings with one array entry for each tweet.
-  Please do not add any preamble before or any text after the JSON structure.
-  Only respond with valid JSON data and no other text.")
+
+  Before returning the JSON tweets structure please expand upon and summarize the topic and the points from it that will make engaging tweets.")
+
+;   Please do not add any preamble before or any text after the JSON structure.
+;  Only respond with valid JSON data and no other text.
+
+(def prompt-good-tweets
+  "Thinking like a smart and thoughtful person with a large following on Twitter and a strong social media presence, what are some tips for writing tweets that get a lot of likes, retweets, and are widely shared? Also what are the main categories of these types of tweets?")
+
+(def prompt-assistant-good-tweets
+  "Sure, here are some tips for writing tweets that resonate with your audience and get a lot of engagement:
+
+1. **Be Authentic**: Share your genuine thoughts, experiences, and insights. Authenticity often attracts more engagement.
+
+2. **Provide Value**: Offer useful tips, advice, or solutions related to your niche that your followers can benefit from.
+
+3. **Use Visuals**: Incorporate eye-catching images, GIFs, or videos that complement your tweet's message.
+
+4. **Use Humor**: Incorporate clever humor or witty remarks, but make sure it aligns with your brand's tone and values.
+
+5. **Pose Questions**: Ask thought-provoking questions that encourage discussions and replies.
+
+6. **Share Success Stories**: Highlight success stories, either your own or from your community, to inspire and motivate your followers.
+
+7. **Use Quotes**: Share insightful or motivational quotes that resonate with your audience.
+
+8. **Share Personal Stories**: Open up about your experiences, challenges, and lessons learned. People often connect with personal stories.
+
+9. **Stay Timely**: Leverage current events, trending topics, or relevant hashtags to increase your tweet's visibility.
+
+10. **Short and Snappy**: Keep your tweets concise, using clear language that's easy to understand.
+
+Main categories of engaging tweets:
+
+1. **Educational**: Share tips, tricks, and insights related to your expertise that your followers can learn from.
+
+2. **Motivational**: Share inspirational quotes, personal anecdotes, or success stories that uplift your audience.
+
+3. **Behind-the-Scenes**: Offer a glimpse into your creative process, projects you're working on, or a day in your life.
+
+4. **Controversial/Thought-Provoking**: Share unconventional opinions or thought-provoking statements that spark discussions.
+
+5. **Polls and Questions**: Engage your audience by asking questions or running polls on topics of interest.
+
+6. **Announcements and Updates**: Share news about your projects, achievements, or upcoming events.
+
+7. **Collaborations**: Tease or announce collaborations with other creators or businesses to generate excitement.
+
+8. **Interactive Content**: Share puzzles, challenges, or interactive content that encourages your followers to participate.
+
+Remember, consistency is key. Engaging with your audience, responding to comments, and participating in conversations will help you build a strong and active community on Twitter.")
 
 ;Tweet like one of the a greatest minds in the modern world.
 
@@ -70,6 +128,10 @@
           messages [{:role "system"
                      :content role}
                     {:role "user"
+                     :content prompt-good-tweets}
+                    {:role "assistant"
+                     :content prompt-assistant-good-tweets}
+                    {:role "user"
                      :content (str tweets-prompt "\n\n" topic-prompt)}
                     {:role "assistant"
                      :content "Ok, I'm ready for the topic that I will use to generate new tweets in the style of the tweets you have given me as a reference."}
@@ -85,9 +147,11 @@
                                  :messages messages
                                  ;:functions [{:name "set_tweets" :parameters schema}]
                                  ;:function_call {:name "set_tweets"}
-                                 :temperature 1}))]
+                                 :temperature 1}))
+          output (j/get-in completion [:data :choices 0 :message :content])]
     (js/console.log "Done generating tweets.")
-    (j/get-in completion [:data :choices 0 :message :content])))
+    (js/console.log output)
+    output))
 
 (def re-json-array (js/RegExp. "\\[(.*)\\]" "s"))
 
@@ -122,6 +186,7 @@
                                     :result parsed
                                     :time (-> (- done start) (/ 1000) int)})
                             nil 2))
+        (js/console.log "user-data:" user-data)
         (when tweets
           (set-user-data! user
                           (j/update-in! user-data
